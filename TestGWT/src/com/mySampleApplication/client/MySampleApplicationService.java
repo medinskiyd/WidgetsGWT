d12056f541990d73662f9d886e01c20814d46539
dmme1016 <medinskiy_d@ukr.net>
2016-10-27 10:45:56 +0300

    add HTTP Servlet (JSON)

--- TestGWT/src/com/mySampleApplication/client/MySampleApplicationService.java
+++ TestGWT/src/com/mySampleApplication/client/MySampleApplicationService.java
@@ -13,7 +13,7 @@ public interface MySampleApplicationService extends RemoteService {
      * Utility/Convenience class.
      * Use MySampleApplicationService.App.getInstance() to access static instance of MySampleApplicationServiceAsync
      */
-    public static class App {
+    class App {
         private static MySampleApplicationServiceAsync ourInstance = GWT.create(MySampleApplicationService.class);
 
         public static synchronized MySampleApplicationServiceAsync getInstance() {
